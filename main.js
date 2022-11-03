99ae72e53525600e0dbe482f0364964466bd7149
jabirhaider25 <91767507+jabirhaider25@users.noreply.github.com>
2022-11-02 19:23:23 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function take_snapshot()
+{
+save('myFilterimage.png')
+}
+function setup(){
+canvas = createCanvas(300,300)
+canvas.center();
+}
+function draw() {
+
+}
+function preload() {
+    
+}
