95cbaf189bb3bcf7b34ee350d8e1a2fd02c10af0
jabirhaider25 <91767507+jabirhaider25@users.noreply.github.com>
2022-11-04 18:53:59 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,35 @@
 function take_snapshot()
 {
-save('myFilterimage.png')
+save('myFilterimage.png');
 }
 function setup(){
-canvas = createCanvas(300,300)
+canvas = createCanvas(300,300);
 canvas.center();
+video = createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+
+poseNet = ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
 }
 function draw() {
-
+image(video, 0, 0, 300, 300);
 }
 function preload() {
-    
+
 }
+
+function modelLoaded() {
+console.log('poseNet is initialized');
+}
+function gotPoses(results) {
+if (results.length > 0);
+{
+console.log(results)
+console.log("lip_x =" + results[0].pose.lip.x);
+console.log("lip_y =" + results[0].pose.lip.y);
+}
+}
+
+
+
